cf8c9bca5ad8efa844f7c4a5148fff29c91d9e69
Kyle <kylezhou2002@hotmail.com>
2014-01-21 02:11:31 -0500

    First commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+digitalroot
+===========
+A program written Java that calculates the digital root of a set of numbers.
+Based on something from the game, 9 Hours, 9 Persons, 9 Doors.
---
+++ src/digitalroot/DigitalRoot.java
@@ -0,0 +1,20 @@
+package digitalroot;
+
+import java.lang.Math;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class DigitalRoot{
+	public static int digitalRoot(int num){
+		return 0; // pass
+	}
+
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in).useDelimiter(" ");
+		ArrayList<Integer> numbers = new ArrayList<>();
+
+		while(true){
+			numbers.add(s.nextInt());
+		}
+		
+	}
+}
