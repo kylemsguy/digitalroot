66daa07d60f3d38018157021cfa13a7d57a91caa
Kyle <kylezhou2002@hotmail.com>
2014-02-12 12:58:51 -0500

    fixed code and started development

--- digitalroot/DigitalRoot.java
+++ digitalroot/DigitalRoot.java
@@ -1,20 +1,20 @@
 package digitalroot;
 
-import java.lang.Math;
 import java.util.ArrayList;
 import java.util.Scanner;
-public class DigitalRoot{
-	public static int digitalRoot(int num){
-		return 0; // pass
-	}
 
-	public static void main(String[] args){
-		Scanner s = new Scanner(System.in).useDelimiter(" ");
+public class DigitalRoot {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
 		ArrayList<Integer> numbers = new ArrayList<>();
-
-		while(true){
-			numbers.add(s.nextInt());
-		}
 		
+		System.out.println("Please enter the numbers one at a time. Enter a number out of range to end. ");
+
+		while (next < ) {
+			String next = s.nextInt();
+			
+			numbers.add(next);
+		}
+		s.close();
 	}
 }
