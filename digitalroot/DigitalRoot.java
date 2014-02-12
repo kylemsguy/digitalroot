0923037baafea8e60cdaa6466327a03d5e07ffcd
Kyle <kylezhou2002@hotmail.com>
2014-02-12 13:11:48 -0500

    made some code changes

--- digitalroot/DigitalRoot.java
+++ digitalroot/DigitalRoot.java
@@ -7,14 +7,16 @@ public class DigitalRoot {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		ArrayList<Integer> numbers = new ArrayList<>();
+		int next = 0;
 		
 		System.out.println("Please enter the numbers one at a time. Enter a number out of range to end. ");
 
-		while (next < ) {
-			String next = s.nextInt();
+		while (next > 0 && next < 10) {
+			next = s.nextInt();
 			
 			numbers.add(next);
 		}
+		
 		s.close();
 	}
 }
